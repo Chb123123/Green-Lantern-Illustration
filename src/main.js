d5734c8f8dd662a14f916cbfba01287bf9ba8147
chb <3277981179@qq.com>
2022-08-16 23:44:00 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -13,6 +13,20 @@ document.addEventListener('contextmenu', function (e) {
 document.addEventListener('selectstart', function (event) {
   event.preventDefault()
 })
+window.onload = function () {
+  document.onkeydown = function () {
+    const e = window.event || arguments[0]
+    console.log(e.keyCode)
+    if (e.keyCode === 123) {
+      return false
+    } else if ((e.ctrlKey) && (e.shiftKey) && (e.keyCode === 67)) {
+      return false
+    } else if ((e.shiftKey) && (e.keyCode === 121)) {
+      return false
+    }
+  }
+}
+
 new Vue({
   router,
   store,
